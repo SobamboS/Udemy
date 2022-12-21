862d19e3b1aff5b9366023e8db6e0d9b2d17f418
SobamboS <sobamboolusola1152@gmail.com>
2022-12-21 01:24:49 +0100

    ""

---
+++ src/Array/ArrayList.java
@@ -0,0 +1,12 @@
+package Array;
+
+import java.util.Scanner;
+
+public class ArrayList<S>{
+    private static Scanner input = new Scanner(System.in);
+    private static int[] baseData = new int[10];
+
+
+
+
+}
---
+++ src/Array/GroceryList.java
@@ -0,0 +1,11 @@
+package Array;
+
+public class GroceryList {
+     public ArrayList<String> groceryList = new ArrayList<String>();
+
+     public void addGroceryItem(String item){
+         groceryList.add(item);
+
+         }
+     }
+}
